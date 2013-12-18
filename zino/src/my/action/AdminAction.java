5005ddf86333660e314da3a6ffd8f9ac3bb2a704
zino <smile.zino@gmail.com>
2013-12-18 16:44:13 +0800

    添加cmd支持

--- zino/src/my/action/AdminAction.java
+++ zino/src/my/action/AdminAction.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import my.beans.User;
 import my.service.Annotation;
 import my.service.RequestContext;
+import my.utils.CommandUtils;
 
 /**
  * 超级管理员执行的action
@@ -44,5 +45,26 @@ public class AdminAction {
 	 */
 	public void delete(RequestContext ctx) throws IOException {
 		//TODO 删除用户的所有资料
+	}
+	
+	/**
+	 * 备份数据到DropBox
+	 * @param ctx
+	 * @throws IOException
+	 */
+	public void backup(RequestContext ctx) throws IOException {
+		String info = CommandUtils.backup();
+		ctx.output_json("info",info);
+	}
+	
+	/**
+	 * 执行shell命令
+	 * @param ctx
+	 * @throws IOException
+	 */
+	public void exec(RequestContext ctx) throws IOException {
+		String cmd = ctx.param("cmd", "");
+		String info = CommandUtils.exec(cmd);
+		ctx.output_json("info",info);
 	}
 }
